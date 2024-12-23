a8df4c60b0f3f34c0e9a6bd920f7a03ea82be996
cjsalinas <47445339+xkaze09@users.noreply.github.com>
2024-12-23 16:43:01 +0800

    Add post data and update home view to render posts

--- blog_app/views.py
+++ blog_app/views.py
@@ -1,8 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+    {
+        'author': 'Christian Salinas',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'December 23, 2024'
+    },
+    {
+        'author': 'Christian Salinas',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'December 24, 2024'
+    }
+]
+
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
+
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html')
