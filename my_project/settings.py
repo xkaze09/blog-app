65eb9e758d0d9043a7599a2cf4d525d4554f54ab
cjsalinas <47445339+xkaze09@users.noreply.github.com>
2025-01-01 19:06:04 +0800

    Add users application to INSTALLED_APPS in settings.py

--- my_project/settings.py
+++ my_project/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'blog_app.apps.BlogAppConfig',
+    'users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
